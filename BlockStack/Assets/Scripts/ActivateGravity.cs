e697769e85f9acf83d13a7e8d41488fca80b48ba
Lincoln Whyte <10820584@uvu.edu>
2026-01-19 22:47:46 -0700

    start block stack

---
+++ BlockStack/Assets/Scripts/ActivateGravity.cs
@@ -0,0 +1,12 @@
+﻿//using System.Collections;
+using UnityEngine;
+//using UnityEngine.Events;
+
+public class ActivateGravity : MonoBehaviour
+    {
+        void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+                Debug.Log("Pressed left-click.");
+        }
+    }
